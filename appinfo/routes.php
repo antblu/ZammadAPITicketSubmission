0c04cae47bb671c7487a7858480ec6cdae052511
antblu <anthony.bluemel@vectordigital.io>
2025-08-26 21:05:31 -0500

    Update routes.php

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,12 +2,12 @@
 declare(strict_types=1);
 
 return [
-    'routes' => [
-        // This serves the page when you click "Help" in the App Navigation
-        ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
-    ],
-    'ocs' => [
-        // OCS endpoint used by the frontend to create tickets
-        ['name' => 'api#tickets', 'url' => '/api/tickets', 'verb' => 'POST'],
-    ],
+  'routes' => [
+    // Route that renders your page when clicking "Help"
+    ['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
+  ],
+  'ocs' => [
+    // OCS endpoint your Vue app calls to create tickets
+    ['name' => 'api#tickets', 'url' => '/api/tickets', 'verb' => 'POST'],
+  ],
 ];
