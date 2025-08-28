83c800b1ad5223ae768f93d3985c7ff26610a88e
antblu <anthony.bluemel@vectordigital.io>
2025-08-28 17:11:39 -0500

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -1,7 +1,5 @@
-<?php
-// Ensures the JS entry is enqueued if not already
-script($_['appId'], 'main');
-?>
+<?php script($_['appId'], 'main'); ?>
+<div id="zammad-help"></div>
 <div class="section">
   <h1 class="page-heading">Help</h1> <!-- <- the text under the nav bar -->
   <div id="zammad-help"></div>       <!-- <- Vue mounts here -->
