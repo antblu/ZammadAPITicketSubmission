e7a3d9841e5a38e6dcd8be13af5fa46dda8ab61c
antblu <anthony.bluemel@vectordigital.io>
2025-08-28 17:49:10 -0500

    Update main.php

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,15 @@
-<?php script($_['appId'], 'main'); ?>
-<div id="zammad-help"></div>
+<?php
+/** @var array $_ */
+script($_['appId'], 'main'); // ensures js/main.js is loaded
+// If you also have a style bundle: style($_['appId'], 'style');
+?>
 <div class="section">
-  <h1 class="page-heading">Help</h1> <!-- <- the text under the nav bar -->
-  <div id="zammad-help"></div>       <!-- <- Vue mounts here -->
+    <h1 class="page-heading"><?php p($l->t('Help')); ?></h1>
+
+    <!-- Vue mounts here -->
+    <div id="zammad-help"></div>
+
+    <p class="hint">
+        <?php p($l->t('Use this form to submit a support ticket to Zammad.')); ?>
+    </p>
 </div>
